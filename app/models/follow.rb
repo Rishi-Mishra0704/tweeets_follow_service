bce0c13858224e3ab7d878e170f7b786cfff610a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-22 00:41:01 +0530

    add model and controller

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,6 +3,4 @@ class Follow < ApplicationRecord
   belongs_to :followee, class_name: 'User', foreign_key: 'followee_id'
 
   validates :follower_id, uniqueness: { scope: :followee_id }
-
-  # Add any additional validations or methods as needed
 end
