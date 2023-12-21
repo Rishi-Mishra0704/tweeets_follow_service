68954166cf6cce84db321a849bbb5e351a1943f7
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-22 00:43:31 +0530

    update routes and controller

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -1,7 +1,23 @@
-class Api::V1::FollowsController < ApplicationController
+class FollowsController < ApplicationController
+
   def create
+    followee = User.find(params[:user_id])
+    if current_user.following?(followee)
+      render json: { detail: 'You are already following this user.' }, status: :bad_request
+    else
+      Follow.create(follower: current_user, followee: followee)
+      render json: { detail: 'User followed successfully.' }, status: :created
+    end
   end
 
   def destroy
+    followee = User.find(params[:user_id])
+    follow = current_user.follows.find_by(followee: followee)
+    if follow
+      follow.destroy
+      render json: { detail: 'User unfollowed successfully.' }, status: :ok
+    else
+      render json: { detail: 'You are not following this user.' }, status: :bad_request
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :follows, only: [ :create, :destroy]
+      resources :users do
+      resource :follow, only: [:create, :destroy], controller: 'follows'
+      end
     end
   end
 
